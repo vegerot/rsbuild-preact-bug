13434c3aa95966fa9fad325d565beeb259b05fd0
Max 😎 Coplan <max.coplan@bytedance.com>
2025-02-25 11:20:58 -0800

    reproduce bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="content">
       <h1>Rsbuild with Preact</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <p>Start building amazing things with Rsbug 🐛🐞.</p>
     </div>
   );
 };
